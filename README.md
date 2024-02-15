b8a0406946ded9e729d75b4c357b51fd2ee8d8c1
Henry Watkins <h.watkins@ucl.ac.uk>
2024-02-15 18:24:55 +0000

    working file read and basic functionality

--- README.md
+++ README.md
@@ -23,5 +23,45 @@ pip install seal
 - input comes from awk
 - output in the form of gui, file or interactive dash (possible with plotly?)
 - cli should follow awk/sed style
-- use relplot,displot,catplot,jointplot as basic types
+- use relplot,displot,catplot,pairplot as basic types
 - f for file input for complicated processing
+- main parameters to seaborn plot - x, y, col ,hue, style, size, kind
+
+kinds of catplot
+Categorical scatterplots:
+
+    stripplot() (with kind="strip"; the default)
+
+    swarmplot() (with kind="swarm")
+
+Categorical distribution plots:
+
+    boxplot() (with kind="box")
+
+    violinplot() (with kind="violin")
+
+    boxenplot() (with kind="boxen")
+
+Categorical estimate plots:
+
+    pointplot() (with kind="point")
+
+    barplot() (with kind="bar")
+
+    countplot() (with kind="count")
+
+displot types
+
+    histplot() (with kind="hist"; the default)
+
+    kdeplot() (with kind="kde")
+
+    ecdfplot() (with kind="ecdf"; univariate-only)
+
+relplot types
+
+
+
+    scatterplot() (with kind="scatter"; the default)
+
+    lineplot() (with kind="line")
--- src/seal/cli/__init__.py
+++ src/seal/cli/__init__.py
@@ -1,3 +1,4 @@
+import re
 from io import StringIO
 
 import click
@@ -14,20 +15,62 @@ from seal.__about__ import __version__
     context_settings={"help_option_names": ["-h", "--help"]}
 )  # , invoke_without_command=True)
 @click.version_option(version=__version__, prog_name="seal")
-@click.argument("script", type=str)
 @click.argument("input_file", type=click.File("r"), default="-")
+@click.option("--program", "-p", type=str, default="~", help="Script string")
 @click.option("--output", "-o", type=str, help="Output file")
 @click.option("--file", "-f", type=click.File("r"), help="Script file")
 @click.option(
     "--separator", "-s", type=str, default=",", help="Separator for input file"
 )
-def seal(script, input_file, output, file, separator):
+def seal(program, input_file, output, file, separator):
     """Terminal-based plotting with seaborn"""
+    if file:
+        program = file.read()
+    plot_type, plot_args, column_names = parse_script(program)
     contents = input_file.read()
-    df = pd.read_csv(StringIO(contents), sep=separator, names=["w", "x", "y", "z", "n"])
-    fig = sns.relplot(data=df, x="x", y="y")
+    name_dict = {"names": column_names} if len(column_names) > 0 else {}
+    df = pd.read_csv(StringIO(contents), sep=separator, **name_dict)
+    fig = plot_type(data=df, **plot_args)
     # if an output file is present then write to it
     if output:
         fig.figure.savefig(output)
     else:
         plt.show()
+
+
+def parse_script(script_string):
+    """Parse the script string and return the dictionary"""
+    # sting of format plot/kind/x/y/hue/col/size/style~col1/col2/col3
+    pattern = r"^[^/]*(?:/[^/]*){7}~"
+    if not bool(re.match(pattern, script_string)):
+        raise ValueError("Invalid script")
+    elements, headers = script_string.strip().split("~")
+    column_names = headers.split("/")
+    elements = elements.split("/")
+    plot_types = {
+        "relplot": sns.relplot,
+        "catplot": sns.catplot,
+        "displot": sns.displot,
+        "pairplot": sns.pairplot,
+    }
+    plot_choice = elements[0] if elements[0] in plot_types.keys() else "relplot"
+    args = ["x", "y", "hue", "col", "size", "kind", "style"]
+    script_data = {i: j for i, j in zip(args, elements[1:]) if j}
+
+    # extra logic - check if kind is in list of support kinds, if column names present check if all columns are in the first part of the string.
+
+    return plot_types[plot_choice], script_data, column_names
+
+
+def validate_format(input_string):
+    # Count the number of '/' characters before '~'
+    slash_count = input_string.count("/")
+    tilde_index = input_string.find("~")
+    pattern = re.compile("^([A-Z][0-9]+)+$")
+    check = pattern.match(input_string)
+    print(check)
+    # Check if there are exactly 7 '/' characters before '~'
+    if slash_count == 7 and tilde_index != -1 and tilde_index > 7:
+        return True
+    else:
+        return False
