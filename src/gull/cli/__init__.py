093e389f48819fa8a0c93bc60fbda4a00ece1e59
Henry Watkins <h.watkins@ucl.ac.uk>
2024-02-29 12:35:51 +0000

    bugfix script read

--- src/gull/cli/__init__.py
+++ src/gull/cli/__init__.py
@@ -20,7 +20,7 @@ sns.set_style("whitegrid")
     "--program",
     "-p",
     type=str,
-    default="~",
+    required=True,
     help="Script string, the paramters and columns to use in plotting",
 )
 @click.option(
@@ -39,7 +39,21 @@ sns.set_style("whitegrid")
     default=",",
     help="Separator for input data, default is ',' ",
 )
-def gull(program, input_file, output, file, separator):
+@click.option(
+    "--columns",
+    "-c",
+    type=str,
+    default=None,
+    help="Column names for the input data, if not present in the file",
+)
+@click.option(
+    "--columns",
+    "-c",
+    type=str,
+    default=None,
+    help="Column names for the input data, if not present in the file",
+)
+def gull(program, columns, input_file, output, file, separator):
     """Terminal-based plotting with seaborn
 
     Plot data from a tabular data file or from stdin, all from the terminal. An ideal companion for awk, grep and other terminal-based processing tools. Uses seaborn as a plotting interface, so for more information see the seaborn website (https://seaborn.pydata.org/)
@@ -58,30 +72,41 @@ def gull(program, input_file, output, file, separator):
 
     Example: to plot a scatterplot of data with column names col1, col2, and coloured by the value in col3.
 
-    gull data.csv -p "plot:relplot,kind:scatter,x:col1,y:col2,hue:col3~col1,col2,col3"
+    gull data.csv -p "plot:relplot,kind:scatter,x:col1,y:col2,hue:col3" -c "col1,col2,col3"
+
+    if no column names are defined via the --columns option, the first line of the  file is assumed to be the column names.
     """
     if file:
         program = file.read()
     try:
-        plot_type, plot_args, column_names = parse_script(program)
+        plot_type, plot_args = parse_script(program)
     except:
         print("Invalid script format, unable to parser program script")
         return
+    column_names = parse_columns(columns)
     contents = input_file.read()
     name_dict = {"names": column_names} if len(column_names) > 0 else {}
     df = pd.read_csv(StringIO(contents), sep=separator, **name_dict)
     fig = plot_type(data=df, **plot_args)
+    plt.xticks(rotation=45, ha="right")
+    plt.tight_layout()
     if output:
         fig.figure.savefig(output)
     else:
         plt.show()
 
 
+def parse_columns(column_string):
+    if column_string:
+        return column_string.strip().split(",")
+    else:
+        return []
+
+
 def parse_script(script_string):
     """Parse the script string and return the dictionary"""
-    # sting of format plot:l1,kind:l2,x:l3,y:l3,hue:l5,col:l6,size:l7,style:l8~col1,col2,col3
-    elements, headers = script_string.strip().split("~")
-    column_names = [i.strip() for i in headers.split(",")]
+    # sting of format plot:l1,kind:l2,x:l3,y:l3,hue:l5,col:l6,size:l7,style:l8
+    elements = script_string.strip()
     elements_dict = dict(
         [
             (i.split(":")[0].strip(), i.split(":")[1].strip())
@@ -100,6 +125,7 @@ def parse_script(script_string):
         else "relplot"
     )
     elements_dict.pop("plot", None)
+    return plot_types[plot_choice], elements_dict
 
 
 def validate_format(input_string):
